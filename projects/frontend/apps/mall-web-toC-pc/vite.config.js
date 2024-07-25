a520b98c1bb9a67fb883a27f4fc8ee16aa6aeb2e
ruan-cat <1219043956@qq.com>
2024-07-25 16:45:45 +0800

    🤔 save-file: 触发部署，查看自定义域的配置；

--- projects/frontend/apps/mall-web-toC-pc/vite.config.js
+++ projects/frontend/apps/mall-web-toC-pc/vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+
 // 引入插件
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
