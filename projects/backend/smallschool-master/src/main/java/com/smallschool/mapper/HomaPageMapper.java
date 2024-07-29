6ce8a5619711d9c1b3c463acf262e7683a9a9284
sanjiaoanshu <107785617+sanjiaoanshu@users.noreply.github.com>
2024-07-29 14:04:05 +0800

    Update HomaPageMapper.java

--- projects/backend/smallschool-master/src/main/java/com/smallschool/mapper/HomaPageMapper.java
+++ projects/backend/smallschool-master/src/main/java/com/smallschool/mapper/HomaPageMapper.java
@@ -1,9 +1,19 @@
 package com.smallschool.mapper;
 
+import com.smallschool.entity.ProductDetails;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
 
-@Mapper
 public interface HomaPageMapper {
+    @Mapper
+    @Select("SELECT * FROM products WHERE sales>50")
+    List<ProductDetails> homeRecommend();
 
+    @Select("SELECT * FROM details WHERE style=#{style}")
+    List<ProductDetails> homeByCategory(String style);
+
+    @Select("SELECT * FROM products WHERE name LIKE CONCAT('%',#{name},'%')")
+    List<ProductDetails> homeSearch(String name);
 }
