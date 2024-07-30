49fa9a01243e7059d6371cef3665e45017d18227
ruan-cat <1219043956@qq.com>
2024-07-30 21:00:39 +0800

    🐞 fix: vite环境变量提示；

--- projects/frontend/apps/mall-admin-web-toB-pc/vite.config.ts
+++ projects/frontend/apps/mall-admin-web-toB-pc/vite.config.ts
@@ -81,7 +81,9 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
       // MOCK 服务
       // env.VITE_MOCK_DEV_SERVER === "true" ? mockDevServerPlugin() : null,
       // 改成无条件开启mock接口。 生产环境也启用mock接口
-      mockDevServerPlugin(),
+      mockDevServerPlugin({
+        build: true,
+      }),
 
       UnoCSS({
         hmrTopLevelAwait: false,
