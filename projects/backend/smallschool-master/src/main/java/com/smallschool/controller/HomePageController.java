b412739cf73c45768d510214ef43d7fb8841860c
ljl <3327885267@qq.com>
2024-08-01 16:21:10 +0800

    主页推荐，搜索代码完善

--- projects/backend/smallschool-master/src/main/java/com/smallschool/controller/HomePageController.java
+++ projects/backend/smallschool-master/src/main/java/com/smallschool/controller/HomePageController.java
@@ -1,15 +1,13 @@
 package com.smallschool.controller;
 
 import com.smallschool.entity.ProductDetails;
+import com.smallschool.entity.ProductRecommend;
 import com.smallschool.service.HomePageService;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,22 +17,24 @@ import java.util.List;
 *   2、分类列表
 *   3、搜索（按照，模糊搜索均可）
 * */
-@Controller
+@RestController
 @RequestMapping("/homePage")
 public class HomePageController {
     @Autowired
     private HomePageService homePageService;
 
     @GetMapping("/recommend")
-    public List<ProductDetails> getRecommend() {
+    public List<ProductRecommend> getRecommend() {
         return homePageService.getRecommend();
     }
+
     @GetMapping("/category/{category}")
-    public List<ProductDetails> getCategory(@PathVariable("category") String category) {
+    public List<ProductRecommend> getCategory(@PathVariable("category") String category) {
         return homePageService.getCategory(category);
     }
+
     @GetMapping("/search")
-    public List<ProductDetails> getSearch(@RequestParam String name) {
+    public List<ProductRecommend> getSearch(@RequestParam String name) {
         return homePageService.getSearch(name);
     }
 }
