cc82f4d606558f151587dfc6af6b6636c1ca67c8
ruan-cat <1219043956@qq.com>
2024-08-02 23:22:55 +0800

    🐞 fix: 照抄，并处理启动bug；

--- projects/frontend/apps/mall-web-toC-pc/vite.config.ts
+++ projects/frontend/apps/mall-web-toC-pc/vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite";
+import { type UserConfig, type ConfigEnv, loadEnv, defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // 引入插件
@@ -8,43 +8,48 @@ import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 import ElementPlus from "unplugin-element-plus/vite";
-// https://vitejs.dev/config/
-export default defineConfig({
-	server: {
-		port: Number(import.meta.env.VITE_APP_BASE_API),
-	},
 
-	plugins: [
-		vue(),
-		// 配置插件
-		AutoImport({
-			resolvers: [ElementPlusResolver()],
-		}),
-		Components({
-			resolvers: [
-				// 配置额，elementPlus采取sass样式配色系统
-				ElementPlusResolver({ importStyle: "sass" }),
-			],
-		}),
-		// 按需定制主题配置
-		ElementPlus({
-			useSource: true,
-		}),
-	],
-	resolve: {
-		alias: {
-			"@": fileURLToPath(new URL("./src", import.meta.url)),
+// https://vitejs.dev/config/
+export default defineConfig(function ({ mode }: ConfigEnv): UserConfig {
+	const env = loadEnv(mode, process.cwd());
+
+	return {
+		server: {
+			port: Number(env.VITE_APP_PORT),
 		},
-	},
-	css: {
-		preprocessorOptions: {
-			scss: {
-				// 自动导入定制化样式文件进行样式覆盖
-				additionalData: `
-          @use "@/styles/element/index.scss" as *;
-          @use "@/styles/var.scss" as *;
-        `,
+
+		plugins: [
+			vue(),
+			// 配置插件
+			AutoImport({
+				resolvers: [ElementPlusResolver()],
+			}),
+			Components({
+				resolvers: [
+					// 配置额，elementPlus采取sass样式配色系统
+					ElementPlusResolver({ importStyle: "sass" }),
+				],
+			}),
+			// 按需定制主题配置
+			ElementPlus({
+				useSource: true,
+			}),
+		],
+		resolve: {
+			alias: {
+				"@": fileURLToPath(new URL("./src", import.meta.url)),
 			},
 		},
-	},
+		css: {
+			preprocessorOptions: {
+				scss: {
+					// 自动导入定制化样式文件进行样式覆盖
+					additionalData: `
+							@use "@/styles/element/index.scss" as *;
+							@use "@/styles/var.scss" as *;
+						`,
+				},
+			},
+		},
+	};
 });
