d0b51b177651e976b261d81b020741dc5ec38852
ruan-cat <1219043956@qq.com>
2024-08-03 00:43:04 +0800

    ✨ feat: 设置商城项目的标题；

--- projects/frontend/apps/mall-web-toC-pc/src/App.vue
+++ projects/frontend/apps/mall-web-toC-pc/src/App.vue
@@ -1,5 +1,10 @@
-<script setup>
+<script setup lang="ts">
 import { RouterView } from "vue-router";
+
+import { useTitle } from "@vueuse/core";
+import { settings } from "@/settings/index";
+
+useTitle(settings.title);
 </script>
 
 <template>
