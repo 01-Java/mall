88352616dd1aa035bb9195d94e61f69c6bfba0ed
ruan-cat <1219043956@qq.com>
2024-12-25 20:33:04 +0800

    🧪 test(nuxt-app): 测试接口是否可以用。经验证，接口正常可用。

--- projects/frontend/apps/nuxt-demo/src/apis/layout.ts
+++ projects/frontend/apps/nuxt-demo/src/apis/layout.ts
@@ -1,6 +1,8 @@
 import { type ApifoxModel_HomeCategoryHead } from "@/models/homeCategoryHead";
 import { type UseAxiosOptions } from "@vueuse/integrations";
 
+import axios from "axios";
+
 // 获取商品分类信息
 // export const getCategoryAPI = () => {
 // 	return request<any, ApifoxModel_HomeCategoryHead>({
@@ -10,7 +12,16 @@ import { type UseAxiosOptions } from "@vueuse/integrations";
 
 /** 获取商品分类信息 */
 export function getCategoryAPI<T = ApifoxModel_HomeCategoryHead>(options?: UseAxiosOptions<T>) {
-	return useAxios<T>("/home/category/head", requestForUseAxios, options);
+	// return useAxios<T>("/home/category/head", requestForUseAxios, options);
+	// 成功 可以做接口请求
+	return useAxios<T>(
+		"/home/category/head",
+		{
+			baseURL: "https://pcapi-xiaotuxian-front-devtest.itheima.net",
+		},
+		axios,
+		options,
+	);
 }
 
 // () => {
