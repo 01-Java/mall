77c28acbc212ce3eabff31e32802ed9c2255bc19
ruan-cat <1219043956@qq.com>
2025-01-09 06:12:01 +0800

    🗑 del: 删除全部的nuxtdemo。

--- projects/frontend/apps/nuxt-demo/src/apis/checkout.ts
+++
@@ -1,8 +0,0 @@
-import request from "@/utils/request";
-
-// 获取订单信息
-export const getCheckInfoAPI = () => {
-	return request({
-		url: "/member/order/pre",
-	});
-};
--- projects/frontend/apps/nuxt-demo/src/plugins/main.ts
+++
@@ -1,6 +0,0 @@
-/**
- * 尝试迁移vite项目的 main.ts 到 nuxt 项目
- * @see https://nuxt.com/docs/guide/directory-structure/plugins#creating-plugins
- * @see https://blog.csdn.net/jiaohuizhuang6019/article/details/135029171
- */
-export default defineNuxtPlugin((nuxtApp) => {});
