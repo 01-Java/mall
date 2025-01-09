003a2d241ee196ff10e2fc0d0ba64a31adb3d0c7
ruan-cat <1219043956@qq.com>
2025-01-09 20:28:55 +0800

    🐳 chore: 删除vitest配置

--- projects/frontend/vitest.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vitest/config";
-export default defineConfig({
-	// test: {
-	// 	reporters: ["html"],
-	// 	outputFile: "./.vitest-reporter-html/index.html",
-	// },
-});
